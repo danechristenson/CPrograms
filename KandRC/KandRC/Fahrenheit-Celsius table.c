bb45801cf9ed5f608611cc3f4e0227a2a18ac581
Dane Christenson <danechristenson@gmail.com>
2016-12-05 23:54:03 -0700

    switched to #defines for upper and lower and step

--- KandRC/KandRC/Fahrenheit-Celsius table.c
+++ KandRC/KandRC/Fahrenheit-Celsius table.c
@@ -3,12 +3,16 @@
 /* print Fahrenheit-Celsius table
 	for fahr = 0, 20, ..., 300 */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 	int fahr;
 
 	printf("Fahrenheit\tCelsius\n");
-	for (fahr = 300; fahr >= 0; fahr -= 20)
+	for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
 	{
 		printf("%6.1d\t\t %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 	}
