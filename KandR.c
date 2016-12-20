797546f65063751e85dd4af17514a11fa9e3d03a
Dane Christenson <danechristenson@gmail.com>
2016-12-19 21:43:27 -0700

    started writing setbits method. further study is required

--- KandR.c
+++ KandR.c
@@ -5,3 +5,15 @@ int main(){
     getchar();
     return 0;
 }
+
+/* getbits: get n bits from position p */
+unsigned getbits(unsigned x, int p, int n)
+{
+    return (x  >> (p + 1 - n)) & ~(~0 << n );
+}
+
+/* setbits: return x with n bits at position p */
+unsigned setbits(unsigned x, int p, int n)
+{
+    return( x >> (p + 1 - n)) & ~(~0 << n);
+}
