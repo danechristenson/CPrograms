c911c896b78e6453cd34f3a25eecd9bdac8fa56b
Dane Christenson <danechristenson@gmail.com>
2017-01-03 21:34:35 -0700

    started chapter 4. added the new getline function

---
+++ code/3-3.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/* atoi: convert s to interger; v2 */
+int atoiv2(char s[])
+{
+    int i, n, sign;
+    
+    for(i = 0;isspace(s[i]); i++) /* skip white space */
+    ;
+    sign = (s[i] == '-') ? -1:1;
+    if(s[i] == '+' || s[i] == '-') /* skip sign */
+    i++;
+    
+    for (n = 0; isdigit(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+    return sign * n;
+}
+
+// shellsort: sort v[0] ... v[n-1] into increasing order
+void shellsort(int v[], int n) 
+{
+    int gap, i, j, temp;
+    
+    for (gap = n/2; gap > 0; gap /= 2)
+    {
+        
+        for(i = gap; i < n; i ++)
+        {
+            for (j = i - gap; j >= 0 && v[j] > v[j+gap]; j -= gap) 
+            {
+                temp = v[j];
+                v[j] = v[j+gap];
+                v[j+gap] = temp;
+            }
+        }
+    }
+}
+
+// reverse: reverse string s in place
+void reverse(char s[])
+{
+    int c, i , j;
+    
+    for ( i = 0, j = strlen(s)-1; i < j; i++, j--) 
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+// itoa: convert n to characters in s 
+void itoa( int n, char s[])
+{
+    int i, sign;
+    
+    if((sign = n ) < 0) //record sign
+    n = -n; //make n positive
+    i = 0;
+    
+    do{ // generate digits in reverse order
+        s[i++] = n % 10 + '0'; // get next digit
+    } while ((n /= 10) > 0); //delete it
+    
+    if(sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+void expand(s1[], s2[])
+{
+    
+}
+
+itob( int n, char s[], int b)
+{
+    
+}
--- code/KandR.c
+++ code/KandR.c
@@ -1,76 +1,56 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
-/* atoi: convert s to interger; v2 */
-int atoiv2(char s[])
-{
-    int i, n, sign;
-    
-    for(i = 0;isspace(s[i]); i++) /* skip white space */
-    ;
-    sign = (s[i] == '-') ? -1:1;
-    if(s[i] == '+' || s[i] == '-') /* skip sign */
-    i++;
-    
-    for (n = 0; isdigit(s[i]); i++)
-        n = 10 * n + (s[i] - '0');
-    return sign * n;
-}
+#define MAXLINE 1000 // max input line length
 
-// shellsort: sort v[0] ... v[n-1] into increasing order
-void shellsort(int v[], int n) 
+int getline2(char line[], int max);
+int strindex(char source[], char searchfor[]);
+
+char pattern[] = "ould"; // pattern to seach for
+
+// find all lines matching the pattern
+main()
 {
-    int gap, i, j, temp;
+    char line[MAXLINE];
+    int found = 0;
     
-    for (gap = n/2; gap > 0; gap /= 2)
-    {
-        
-        for(i = gap; i < n; i ++)
+    while (getline2(line, MAXLINE) > 0){
+        if (strindex(line, pattern) >= 0) 
         {
-            for (j = i - gap; j >= 0 && v[j] > v[j+gap]; j -= gap) 
-            {
-                temp = v[j];
-                v[j] = v[j+gap];
-                v[j+gap] = temp;
-            }
+            printf("%s" , line);
+            found++;
         }
     }
+    
+    return found;
 }
 
-// reverse: reverse string s in place
-void reverse(char s[])
+// getline: get line into s, return length
+int getline2(char s[], int lim)
 {
-    int c, i , j;
+    int c, i;
     
-    for ( i = 0, j = strlen(s)-1; i < j; i++, j--) 
-    {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
-}
-
-// itoa: convert n to characters in s 
-void itoa( int n, char s[])
-{
-    int i, sign;
-    
-    if((sign = n ) < 0) //record sign
-    n = -n; //make n positive
     i = 0;
-    
-    do{ // generate digits in reverse order
-        s[i++] = n % 10 + '0'; // get next digit
-    } while ((n /= 10) > 0); //delete it
-    
-    if(sign < 0)
-        s[i++] = '-';
+    while(--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if(c == '\n')
+        s[i++] = c;
     s[i] = '\0';
-    reverse(s);
+    return i;
 }
 
-void main()
+//strindex: return index of t in s, -1 if none
+int strindex(char s[], char t[])
 {
-    return 0;
-}
+    int i, j, k;
+    
+    for( i = 0; s[i] != '\0'; i++)
+    { 
+        for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+        {
+            ;
+        }
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+    }
